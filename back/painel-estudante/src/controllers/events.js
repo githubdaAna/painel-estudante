03712b751b18883b250c55af256652a37f673ace
githubdaAna <kristinnacruzz@gmail.com>
2023-03-09 11:39:38 -0300

    Adicionando rotas e controladores para lidar com provas/trabalhos

---
+++ back/painel-estudante/src/controllers/events.js
@@ -0,0 +1,27 @@
+const pool = require('../connection');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const secret = require('../secret');
+
+const addEvent = (req, res) => {
+
+}
+
+const viewEvent = (req, res) => {
+
+}
+
+const deleteEvent = (req, res) => {
+
+}
+
+const updateEvent = (req, res) => {
+
+}
+
+module.exports = {
+    addEvent,
+    viewEvent,
+    deleteEvent,
+    updateEvent
+}
