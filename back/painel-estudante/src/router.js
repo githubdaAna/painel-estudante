bf26a166b6899649fa0c727c8b26c16e2ea13b5b
githubdaAna <kristinnacruzz@gmail.com>
2023-03-09 11:44:06 -0300

    Adicionando rotas

--- back/painel-estudante/src/router.js
+++ back/painel-estudante/src/router.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const { addUser, loginUser, updateUser, viewUser } = require('./controllers/users');
+const { addUser, loginUser, updateUser, viewUser, viewTeacher, addTeacher } = require('./controllers/users');
+const { viewSubject, addSubject, updateSubject, deleteSubject } = require('./controllers/subjects')
 const checkUserAuth = require('./middleware/authentication');
 const router = express();
 
@@ -12,5 +13,11 @@ router.use(checkUserAuth);
 //rotas protegidas
 router.get("user", viewUser);
 router.put("/user", updateUser);
+router.get("user/teacher", viewTeacher);
+router.post("/user/teacher", addTeacher);
+router.get("/subject", viewSubject);
+router.post("/subject", addSubject);
+router.put("/subject", updateSubject);
+router.delete("/subject", deleteSubject);
 
 module.exports = router;
